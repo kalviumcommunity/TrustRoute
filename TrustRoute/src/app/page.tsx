7921ff63a25ae37f23a75e1908185daae8bcceaf
balaji-r-2007 <balaji648balaji@gmail.com>
2026-01-22 14:19:15 +0530

    Created Frontend

---
+++ TrustRoute/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <main style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <h1>Trustroute</h1>
+    </main>
+  );
+}
